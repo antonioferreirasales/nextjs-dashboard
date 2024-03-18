139ed63c92dd0998162ec5500e8248a1301c5f2f
Antônio Sales <antoniosales1003@gmail.com>
2024-03-18 16:52:34 -0300

    feat: 09-ch add suspense to other dashboard components

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -34,13 +34,22 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   noStore();
+
   try {
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+    console.log('Data fetch completed after 3 seconds.');
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
